cd8c664083d40a36ea895396e2d55f23f2140f37
kushagragpt99 <kushagra.gpt99@gmail.com>
2019-03-29 18:02:00 +0530

    newton rhapson added

--- main.m
+++ main.m
@@ -5,21 +5,21 @@ format long;
 % Y = @(t) Y(t); % function description for Y, e.g., Y = @(t) 3*sin(2*pi*t)
 % dXdt = @(t) X’(t); % derivative for X(t), e.g., X = @(t) -4*pi*sin(2*pi*t)
 % dYdt = @(t) Y’(t); % derivative for Y(t), e.g., Y = @(t) 6*pi*cos(2*pi*t)
-X = @(t) 2*cos(2*pi*t);
-Y = @(t) 3*sin(2*pi*t);
-dXdt = @(t) -4*pi*sin(2*pi*t);
-dYdt = @(t) 6*pi*cos(2*pi*t);
+X = @(t) 2*cos(2*pi*t)*(1-cos(2*pi*t));
+Y = @(t) 2*sin(2*pi*t)*(1-cos(2*pi*t));
+dXdt = @(t) 4*pi*(sin(4*pi*t)-sin(2*pi*t));
+dYdt = @(t) 4*pi*(cos(2*pi*t)-cos(4*pi*t));
 
 % point to be projected
-x0 = 2; % the value for x0 goes here
-y0 = 3; % the value for y0 goes here
+x0 = -4; % the value for x0 goes here
+y0 = 0; % the value for y0 goes here
 % desired accuracy
-eps = power(10,-5);  % an epsilon specifying desired absolute error in the
+eps = power(10,-14);  % an epsilon specifying desired absolute error in the
 % cosine of the angle made by the projection vector
 % [X(tc)-x0, Y(tc)-yc] with the tangent line to the
 % curve at tc, i.e., [X’(tc), Y’(tc)]
 tic;
 tc = orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps);
 toc;
-disp(tc);
+%disp(tc);
 [X(tc) Y(tc)]
--- orthoProjectionOnCurve.m
+++ orthoProjectionOnCurve.m
@@ -1,27 +1,45 @@
 function tc = orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
 
+format long;
+f = @(t) (((X(t)-x0)^2)+((Y(t)-y0)^2)); % square of distance function
+dfdt = @(t) ((X(t)-x0)*dXdt(t))+((Y(t)-y0)*dYdt(t)); % first derivative of f
+ % Finding the double derivative of f
+syms t
+d2fdt2 = diff(((X(t)-x0)*dXdt(t))+((Y(t)-y0)*dYdt(t)),1);
+d2fdt2 = matlabFunction(d2fdt2); % second derivative of f
 %gradient descent
-sabse_chota=power(10,10);
+smallest=power(10,10);
 tc=0;
 t_corr=0;
-alpha=0.00001;
-for i=[1:100]
-    anish_ki_maa=rand;
-    no_iter=0;
-    change = 1;
-    while no_iter<power(10,5) && change > power(10,-10)
-        chuna = power(x0 - X(anish_ki_maa),2) + power(y0-Y(anish_ki_maa),2);
-        update = 2*(X(anish_ki_maa) - x0)*dXdt(anish_ki_maa) + 2*(Y(anish_ki_maa) - y0)*dYdt(anish_ki_maa);
-        anish_ki_maa = anish_ki_maa - alpha*update;
-        change=abs(alpha*update);
-        no_iter = no_iter+1;
-    end
-    if sabse_chota > chuna
-        t_corr=anish_ki_maa;
-        sabse_chota=chuna;
-    end
+alpha=0.001;
+ 
+  for i=[1:1000]
+      t_trial=rand;
+      no_iter=0;
+      change = 1;
+      while no_iter<power(10,3) && change > power(10,-14)
+          loss = f(t_trial);
+          update = 2*dfdt(t_trial);
+          t_trial = t_trial - alpha*update;
+          change=abs(alpha*update);
+          no_iter = no_iter+1;
+      end
+      if smallest > loss
+          t_corr=t_trial;
+          smallest=loss;
+      end
+      tc=t_corr;
+  end
+  
+  t=t_corr;
+  no_iter=0;
+  change=power(10,10);
+  while no_iter<power(10,4) && change > power(10,-15)
+      t=t-(dfdt(t)/d2fdt2(t));
+      change=abs(dfdt(t)/d2fdt2(t));
+      no_iter=no_iter+1;
+  end
+  tc=t;
+  %disp(f(tc));
 end
-disp(sabse_chota);
-tc=t_corr;
-    
-
+  
